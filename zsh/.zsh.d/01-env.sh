88e09736290999c8f944b58c876404047b3e6fc6
Mustafa Simav <ms@msimav.net>
2017-05-03 23:14:26 +0300

    Handle $TERM properly

--- zsh/.zsh.d/01-env.sh
+++ zsh/.zsh.d/01-env.sh
@@ -1,6 +1,13 @@
 export EDITOR="vim"
 export VISUAL="sublime"
-export TERM="xterm-256color"
 export PATH="$HOME/bin:$PATH"
 
 # export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+if [ "$COLORTERM" = "xfce4-terminal" ]; then
+    export TERM=xterm-256color
+fi
+
+if [ -n "$TMUX" ]; then
+    export TERM=screen-256color
+fi
