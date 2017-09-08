11411f74cc3594108620753c1153a9ccce079c6d
Mustafa Simav <mustafa@vngrs.com>
2017-09-08 12:55:16 +0300

    Add fasd aliases

--- zsh/.zsh.d/80-fasd.sh
+++ zsh/.zsh.d/80-fasd.sh
@@ -1,3 +1,9 @@
 if [ -x "$(command -v fasd)" ]; then
  eval "$(fasd --init posix-alias zsh-hook)"
+ alias v='f -e vim'
+ if [ -x "$(command -v xdg-open)" ]; then
+   alias o='a -e xdg-open'
+ else
+   alias o='a -e open'
+ fi
 fi
