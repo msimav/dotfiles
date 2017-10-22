ac563ce83e04ad0c56b4dfb80879e3ea001a164c
Mustafa Simav <mustafa@vngrs.com>
2017-10-22 11:21:53 +0300

    Add tm function

--- zsh/.zsh.d/80-fzf.sh
+++ zsh/.zsh.d/80-fzf.sh
@@ -1,4 +1,11 @@
 if [ -x "$(command -v fzf)" ]; then
-  export FZF_DEFAULT_COMMAND='ag -g ""'
+  export FZF_DEFAULT_COMMAND='ag --ignore-dir .git --hidden -g "" 2> /dev/null'
   vimf() { vim $(fzf) }
+  tm() {
+    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+    if [ $1 ]; then
+      tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+    fi
+    session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+  }
 fi
