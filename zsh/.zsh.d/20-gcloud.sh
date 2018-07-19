dd131c0e41e2d47b0749714b12947b6ecc6561ff
Mustafa Simav <ms@msimav.net>
2018-07-19 14:11:47 +0300

    zsh google-cloud-sdk setup

---
+++ zsh/.zsh.d/20-gcloud.sh
@@ -0,0 +1,7 @@
+if [ -f '/usr/local/opt/google-cloud-sdk/path.zsh.inc' ]; then
+  source '/usr/local/opt/google-cloud-sdk/path.zsh.inc'
+fi
+
+if [ -f '/usr/local/opt/google-cloud-sdk/completion.zsh.inc' ]; then
+  source '/usr/local/opt/google-cloud-sdk/completion.zsh.inc'
+fi
