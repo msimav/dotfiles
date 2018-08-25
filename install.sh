2914860c19797296e95f1c022f218f0735c56d3d
Mustafa Simav <ms@msimav.net>
2018-08-25 12:51:26 +0300

    vscode initial config

--- install.sh
+++ install.sh
@@ -1,38 +1,42 @@
 #!/bin/sh
 
-if ! [ -x "$(command -v stow)" ]; then
-  printf "Installing stow via: "
-  case "$(uname -o)" in
-  Darwin)
-    printf "brew install stow\n"
-    brew install stow
-    ;;
-  GNU/Linux)
-    . /etc/os-release
-    case $NAME in
-      NixOS)
-        printf "nix-env -i stow\n"
-        nix-env -i stow
-        ;;
-      Ubuntu)
-        printf "apt-get install stow\n"
-        sudo apt-get install stow
-        ;;
-      Arch*)
-        printf "pacman -S stow\n"
-        sudo pacman -S stow
-        ;;
-      *)
-        printf "\rDon't support Distro $NAME\n"
-        exit 1
-        ;;
-    esac
-    ;;
-  *)
-    printf "\rDon't support OS $(uname -o)\n"
-    exit 1
-    ;;
+case "$(uname -o)" in
+Darwin)
+  export INSTALL_CMD="brew install stow"
+  export KITTY="$HOME/Library/Preferences/kitty"
+  export SUBLIME="$HOME/Library/Application Support/Sublime Text 3/Packages"
+  export VSCODE="$HOME/Library/Application Support/Code/User"
+  ;;
+GNU/Linux)
+  . /etc/os-release
+  export KITTY="$HOME/.config/kitty"
+  export SUBLIME="$HOME/.config/sublime-text-3/Packages"
+  export VSCODE="$HOME/.config/code/User"
+  case $NAME in
+    NixOS)
+      export INSTALL_CMD="nix-env -i stow"
+      ;;
+    Ubuntu)
+      export INSTALL_CMD="sudo apt-get install stow"
+      ;;
+    Arch*)
+      export INSTALL_CMD="sudo pacman -S stow"
+      ;;
+    *)
+      printf "\rDon't support Distro $NAME\n"
+      exit 1
+      ;;
   esac
+  ;;
+*)
+  printf "\rDon't support OS $(uname -o)\n"
+  exit 1
+  ;;
+esac
+
+if ! [ -x "$(command -v stow)" ]; then
+  echo "Installing stow via: $INSTALL_CMD"
+  sh -c "$INSTALL_CMD"
 fi
 
 set -x
@@ -41,14 +45,6 @@ stow -t ~ sbt
 stow -t ~ tmux
 stow -t ~ zsh
 stow -t ~/.config/nvim nvim
-
-case "$(uname -o)" in
-Darwin)
-  stow -t "$HOME/Library/Application Support/Sublime Text 3/Packages" sublime
-  stow -t "$HOME/Library/Preferences/kitty" kitty
-  ;;
-GNU/Linux)
-  stow -t "$HOME/.config/sublime-text-3/Packages" sublime
-  stow -t "$HOME/.config/kitty" kitty
-  ;;
-esac
+mkdir -p "$KITTY" && stow -t "$KITTY" kitty
+test -d "$SUBLIME" && stow -t "$SUBLIME" sublime
+test -d "$VSCODE" && stow -t "$VSCODE" vscode
